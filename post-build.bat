65417f8bd99028576470447877476c00376cc436
BB pezsgő <bazsi5fel2@gmail.com>
2023-06-11 09:26:08 +0200

    yes

--- post-build.bat
+++ post-build.bat
@@ -1,6 +1,6 @@
 @echo off
-set source="bin\debug\net6.0\*"
-copy %source% ..\Debugger\
-echo Copyed to "..\Debugger\"
+set source="D:\Program Files\BBCodeProject\BBCode\bin\Debug\net6.0\*"
+copy %source% "D:\Program Files\BBCodeProject\Debugger\"
+echo Copyed to "D:\Program Files\BBCodeProject\Debugger\"
 copy %source% C:\Users\bazsi\Documents\GitHub\InterpreterVSCodeExtension\
 echo Copyed to "C:\Users\bazsi\Documents\GitHub\InterpreterVSCodeExtension\"

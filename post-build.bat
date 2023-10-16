0e3201444168fa0d84112dae50ff3b001d0987b7
BB pezsgő <bazsi5fel2@gmail.com>
2023-10-16 06:07:00 +0200

    nothing?

--- post-build.bat
+++ post-build.bat
@@ -1,6 +1,3 @@
 @echo off
-set source="D:\Program Files\BBCodeProject\BBCode\bin\Debug\net6.0\*"
-copy %source% "D:\Program Files\BBCodeProject\Debugger\"
-echo Copyed to "D:\Program Files\BBCodeProject\Debugger\"
-copy %source% C:\Users\bazsi\Documents\GitHub\InterpreterVSCodeExtension\
-echo Copyed to "C:\Users\bazsi\Documents\GitHub\InterpreterVSCodeExtension\"
+
+robocopy "D:\Program Files\BBCodeProject\BBCode\bin\ " "C:\Users\bazsi\Documents\GitHub\InterpreterVSCodeExtension\interpreter\ " /E

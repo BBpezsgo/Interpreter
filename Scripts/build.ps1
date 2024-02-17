b5c3297f8d272f5b5e5d1a50a4214c2867ce4499
BB pezsgő <bazsi5fel2@gmail.com>
2024-02-17 08:17:42 +0100

    Remove post-build.bat

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -5,11 +5,6 @@ if (Test-Path -Path ./Published)
     Remove-Item -LiteralPath ./Published -Force -Recurse
 }
 
-if (Test-Path -Path ./Interpreter/post-build.bat)
-{
-    Set-Content -Path "./Interpreter/post-build.bat" -Value ""
-}
-
 dotnet publish ./Interpreter/BBCodeInterpreter.csproj --configuration Release --runtime win-x64  -p:PublishAot=false   --self-contained true   -p:PublishTrimmed=true  -p:PublishSingleFile=true   --output ./Published/Windows_x64_RuntimeIndependent
 dotnet publish ./Interpreter/BBCodeInterpreter.csproj --configuration Release --runtime win-x64  -p:PublishAot=false   --self-contained false  -p:PublishTrimmed=false -p:PublishSingleFile=false  --output ./Published/Windows_x64_RuntimeDependent
 dotnet publish ./Interpreter/BBCodeInterpreter.csproj --configuration Release --runtime win-x64  -p:PublishAot=true    --self-contained false  -p:PublishTrimmed=      -p:PublishSingleFile=false  --output ./Published/Windows_x64_AOT -p:DefineConstants=AOT

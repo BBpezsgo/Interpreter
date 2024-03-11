e6f9fc0d74cee96bdcafdc8f1965e5ae55d7df2b
BB pezsgő <bazsi5fel2@gmail.com>
2024-03-11 09:02:36 +0100

    added more brainfuck minification thing

--- Source/Brainfuck/Compiler/Minifier.cs
+++ Source/Brainfuck/Compiler/Minifier.cs
@@ -28,13 +28,10 @@ public static class Minifier
             if (Utils.TryReplace(ref result, "-+", string.Empty))
             { continue; }
 
-            if (Utils.TryReplace(ref result, "[-][-]", "[-]"))
-            { continue; }
-
-            if (Utils.TryReplace(ref result, "][-]", "]"))
-            { continue; }
-
             if (RemoveRedundantClears(ref result))
+            { continue; }
+
+            if (RemoveRedundantBranches(ref result))
             { continue; }
 
             break;
@@ -94,6 +91,62 @@ public static class Minifier
                 '.' => alreadyThere,
                 _ => PredictedNumber<int>.Unknown,
             };
+        }
+
+        return false;
+    }
+
+    static bool RemoveRedundantBranches(ref string result)
+    {
+        ReadOnlySpan<char> span = result.AsSpan();
+        bool res = RemoveRedundantBranches(ref span);
+        result = span.ToString();
+        return res;
+    }
+
+    static bool RemoveRedundantBranches(ref ReadOnlySpan<char> result)
+    {
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (result[i] == ']' &&
+                i + 2 < result.Length &&
+                result.Slice(i, 2).SequenceEqual("]["))
+            {
+                ReadOnlySpan<char> slice = result[i..];
+                slice = slice[1..];
+                int depth = 0;
+                int end = -1;
+                for (int j = 0; j < slice.Length; j++)
+                {
+                    if (slice[j] == '[')
+                    {
+                        depth++;
+                    }
+                    else if (slice[j] == ']')
+                    {
+                        depth--;
+                        if (depth == 0)
+                        {
+                            end = j;
+                            break;
+                        }
+                    }
+                }
+
+                if (end == -1)
+                { return false; }
+
+                slice = slice[(end + 1)..];
+                int removed = end + 1;
+
+                Span<char> final = new char[result.Length - removed];
+
+                result[..(i + 1)].CopyTo(final);
+                slice.CopyTo(final[(i + 1)..]);
+
+                result = final;
+                return true;
+            }
         }
 
         return false;

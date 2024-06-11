119d9b87986b1cc3defad183940b9b9470bf0581
BB <bazsi5fel2@gmail.com>
2024-06-11 07:21:45 +0200

    remove old scripts

--- Scripts/upload.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set UPLOADER="D:\Program Files\BBCodeProject\Uploader\"
-cd %UPLOADER%
-node ./out/app.js "Download Content" %1
--- Scripts/vscode.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set DOTNET_ROOT=%USERPROFILE%\dotnet
-set PATH=%USERPROFILE%\dotnet;%PATH%
-set DOTNET_MULTILEVEL_LOOKUP=0
-code ..

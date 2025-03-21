e712f4c4dead16b145d05aaaf5fc53064faa3083
BB <bazsi5fel2@gmail.com>
2025-03-21 08:06:58 +0100

    Fix tests for the examples

--- Tests/ExampleTests.cs
+++ Tests/ExampleTests.cs
@@ -11,7 +11,8 @@ public class ExampleTests
     public void HelloWorld()
     {
         StringBuilder output = new();
-        Console.SetOut(new StringWriter(output));
+        using StringWriter writer = new(output);
+        Console.SetOut(writer);
 
         Examples.HelloWorld.Run();
 
@@ -24,14 +25,15 @@ public class ExampleTests
     public void ExternalFunctions()
     {
         StringBuilder output = new();
-        Console.SetOut(new StringWriter(output));
+        using StringWriter writer = new(output);
+        Console.SetOut(writer);
 
         Examples.ExternalFunctions.Run();
 
         Assert.AreEqual(
-            "This was called from the script!!!\n" +
-            "This was called with these arguments: -3, 8\n" +
-            "This was called with these arguments: 76, 4 and the result is 80\n",
+            $"This was called from the script!!!{writer.NewLine}" +
+            $"This was called with these arguments: -3, 8{writer.NewLine}" +
+            $"This was called with these arguments: 76, 4 and the result is 80{writer.NewLine}",
             output.ToString());
     }
 
@@ -39,7 +41,8 @@ public class ExampleTests
     public void ExposedFunctions()
     {
         StringBuilder output = new();
-        Console.SetOut(new StringWriter(output));
+        using StringWriter writer = new(output);
+        Console.SetOut(writer);
 
         Examples.ExposedFunctions.Run();
 
@@ -47,7 +50,7 @@ public class ExampleTests
             "This was called from C#!!!\r\n" +
             "This was called with these arguments: 4, -17\r\n" +
             "This was called with these arguments: 64, 2 and the result is 66\r\n" +
-            "Function \"with_return_value\" returned 66\n",
+            $"Function \"with_return_value\" returned 66{writer.NewLine}",
             output.ToString());
     }
 }
